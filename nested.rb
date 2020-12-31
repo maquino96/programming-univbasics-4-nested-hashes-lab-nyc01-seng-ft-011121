1b2de87015f7ada1d651f9af09cad7749d7c4823
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-31 03:16:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,8 +132,7 @@ def adding_to_dennis
       }
     }
 
-programmer_hash[:dennis_ritchie][languages].push("Assembly")
-puts programmer_hash
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
 
-adding_to_dennis
